b40e847d18c9663be2cecfc7bc92cc944573a41b
prakhardawar_05 <73894175+Prakhar-ai-cloud@users.noreply.github.com>
2026-07-22 17:52:52 +0530

    Time: 0 ms (100.00%) | Memory: 35.8 MB (30.45%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        // can use prefix and postfix sum.
+        // better maintain total sum then check two sides;
+        int sum = 0;
+        for(int i=0;i<nums.size();i++){
+            sum+= nums[i];
+        }
+        int running = 0;
+        for(int i=0 ; i<nums.size();i++){
+            if(running == sum-running-nums[i]) return i;
+            running += nums[i];
+        }
+        return -1;
+    }
+};
