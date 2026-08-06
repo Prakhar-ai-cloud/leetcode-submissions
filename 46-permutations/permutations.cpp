a3dd6f854e78e48f82695f782a01a8c27aaaa1ec
prakhardawar_05 <73894175+Prakhar-ai-cloud@users.noreply.github.com>
2026-08-06 16:12:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (98.40%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void solve(int index, vector<int>& nums, vector<int>& temp, vector<vector<int>>&answers, vector<int>& visited){
+        if(index == nums.size()){
+            if(temp.size()==nums.size()){
+                answers.push_back(temp);
+            }
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(visited[nums[i]+10]) continue;
+            visited[nums[i]+10] = 1;
+            temp.push_back(nums[i]); // take condition;
+            solve(index+1, nums, temp, answers,visited);
+            visited[nums[i]+10] = 0;
+            temp.pop_back();
+        }
+
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        unordered_map<int,vector<int>> map;
+        int n = nums.size();  
+       vector<int> visited(21,0);
+       vector<int> temp;
+       vector<vector<int>> answers;
+       solve(0, nums, temp, answers,visited);
+       return answers;
+    }
+};
